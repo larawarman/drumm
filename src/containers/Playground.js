55cd2f669385737e996e4ac22ceb41b84c222879
larawarman <lara.warman@gmail.com>
2017-07-30 08:52:22 -0400

    refactor: remove unused reference

--- src/containers/Playground.js
+++ src/containers/Playground.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import styled from 'styled-components';
 
 import Playhead from '../components/Playhead';
